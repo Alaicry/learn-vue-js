d818cd38644a2fe1564c173dc9133d07c650c2ce
alaicry <alaicry99@gmail.com>
2024-10-06 17:54:01 +0500

    Начало изучение Vue.JS 3. Сверстаны компоненты Header, CardList и Card

---
+++ App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Card from "./components/Card.vue";
+import CardList from "./components/CardList.vue";
+import Header from "./components/Header.vue";
+import Container from "./UI/Container.vue";
+import Main from "./UI/Main.vue";
+</script>
+
+<template>
+    <Container>
+        <Header />
+        <Main>
+            <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+            <CardList />
+        </Main>
+    </Container>
+</template>
+
+<style scoped></style>
---
+++ UI/Main.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+    <main class="p-10">
+        <slot></slot>
+    </main>
+</template>
+
+<style scoped></style>
